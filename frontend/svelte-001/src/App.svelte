6914c7bc796cc98736bc1f0a866110ef787623ab
Max <max@dfinity.org>
2021-11-17 12:28:18 +0100

    +=login page

--- frontend/svelte-001/src/App.svelte
+++ frontend/svelte-001/src/App.svelte
@@ -1,25 +1,57 @@
 <script lang="ts">
+	import LoginPage from './LoginPage.svelte';
+
 	export let name: string;
+
+	let loggedIn: false;
+	function toggle() {
+		loggedIn = !loggedIn;
+	}
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	{#if loggedIn}
+		<button on:click={toggle}>
+			Log out
+		</button>
+	{:else}
+		<LoginPage bind:loggedIn={loggedIn}>
+		</LoginPage>
+	{/if}
 </main>
 
 <style>
+:global(:root){
+  --widget-border: 25px;
+  --widget-border-radius: 25px;
+  --widget-border-radius-small: 10px;
+  --widget-grey: #282a2d;
+  --text-grey: #aeb7b7;
+  --button-blue: #005fb7;
+}
+
+:global(body) {
+    margin: 0;
+    padding: 0;
+}
+
 	main {
+		/* Svelte defaults: */
 		text-align: center;
 		padding: 1em;
 		max-width: 240px;
 		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
+		/* Imported from pure: */
+    width: 100vw;
+    height: 100vh;
+    display: block;
+    background-color: black;
+    font-family: arial;
+    color: white;
+               /* trouble with overlow.. not sure why.  Hiding is not pretty but it works: */
+               overflow: hidden;
+               margin: 0;
+               padding: 0;
 	}
 
 	@media (min-width: 640px) {
---
+++ frontend/svelte-001/src/LoginPage.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	export let loggedIn = false;
+        function logIn() {
+                loggedIn = true;
+        }
+</script>
+<style>
+login-page > * {
+    text-align: center;
+    margin-left: auto;
+    margin-right: auto;
+}
+button {
+    width: 200px;
+    height: 80px;
+    line-height: 80px;
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+    background-color: #52545a;
+    border: var(--widget-border);
+    border-radius: var(--widget-border-radius);
+    font-size: 30px;
+    color: #aeb7b7;
+}
+</style>
+
+
+       <h1>The Internet Computer</h1>
+       <h2>Network Nervous System</h2>
+       <button on:click={logIn}>Login</button>
+       <span>Beta</span>
+

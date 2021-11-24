2259dc2e3d4f9d1a4dc46b658dbcb47e1e41284a
Max <max@dfinity.org>
2021-11-24 23:40:24 +0100

    Try going direct

--- frontend/svelte-app/frontend/AccountsPage.svelte
+++ frontend/svelte-app/frontend/AccountsPage.svelte
@@ -1,5 +1,9 @@
 <script lang="ts">
   // import ServiceApi from "./api/ServiceApi.ts"
+  import * as nnsApp from "./canisters/nnsDapp/nnsApp.did.js";
+  document.nnsApp = nnsApp;
+  console.log("foo", nnsApp);
+  // document.nnsAppApi = {AccountDetails, AccountIdentifier};
 </script>
 
 
---
+++ frontend/svelte-app/frontend/canisters/nnsDapp/build.sh
@@ -0,0 +1,2 @@
+/home/max/dfn/candid/target/debug/didc bind nnsApp.did -t js > nnsApp.did.js
+/home/max/dfn/candid/target/debug/didc bind nnsApp.did -t ts > nnsApp.did.d.ts

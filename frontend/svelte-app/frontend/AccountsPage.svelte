97c341e5fa66ab3a970c39d941c709b0be10f434
Max <max@dfinity.org>
2021-11-25 11:28:53 +0100

    Expose hook to play with the actor in a browser console

--- frontend/svelte-app/frontend/AccountsPage.svelte
+++ frontend/svelte-app/frontend/AccountsPage.svelte
@@ -1,9 +1,13 @@
 <script lang="ts">
   // import ServiceApi from "./api/ServiceApi.ts"
+  import { Actor, HttpAgent } from "@dfinity/agent";
   import * as nnsApp from "./canisters/nnsDapp/nnsApp.did.js";
   document.nnsApp = nnsApp;
   console.log("foo", nnsApp);
-  // document.nnsAppApi = {AccountDetails, AccountIdentifier};
+  const agent = new HttpAgent();
+  document.icagent = {Actor, HttpAgent};
+  const actor = Actor.createActor(nnsApp.idlFactory, { agent, canisterId: "sbzkb-zqaaa-aaaaa-aaaiq-cai" });
+  document.icactor = actor;
 </script>
 
 
